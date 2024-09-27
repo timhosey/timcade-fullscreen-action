e370a408956d4e49b2a0fa603ca4c4bb12a8df7a
Tim Hosey <insertcointheater@gmail.com>
2024-09-26 19:36:45 -0700

    added action split

--- video_queue.php
+++ video_queue.php
@@ -1,13 +1,18 @@
 <?php
-// Take out get variables
-if ( !isset($_GET['vname']) ) {
-  die('no vname provided in GET');
-}
-
+// Take our get variables, fail if not set
 if ( !isset($_GET['action']) ) {
   die('no action provided in GET');
 }
 
 require_once('settings.php');
+$action = $_GET['action'];
 
-$vname = $_GET['vname'];
+// Add a new item to queue.
+if ($action == 'add') {
+  // Fail if vname isn't set
+  if ( !isset($_GET['vname']) ) {
+    die('no vname provided in GET');
+  }
+  
+  $vname = $_GET['vname'];
+}
