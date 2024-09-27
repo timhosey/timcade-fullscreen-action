33d727b422820cf2ab47923eb705d709204e50a8
Tim Hosey <insertcointheater@gmail.com>
2024-09-27 00:52:32 -0700

    added functionality for removal of the latest entry based on the id understood from the js

--- video_queue.php
+++ video_queue.php
@@ -10,32 +10,51 @@ require_once 'video_queue_lib.php';
 $action = $_GET['action'];
 
 // Add a new item to queue.
-if ($action == 'add') {
-  // Fail if vname isn't set
-  if ( !isset($_GET['vname']) ) {
-    die('no vname provided in GET');
-  }
-  
-  $vname = $_GET['vname'];
+switch($action) {
+  case 'add':
+    // Fail if vname isn't set
+    if ( !isset($_GET['vname']) ) {
+      die('no vname provided in GET');
+    }
+    
+    $vname = $_GET['vname'];
 
-  // Function library for video_queue.php
-  // create or open (if exists) the database
-  $db = new SQLite3('queue.db');
+    // Function library for video_queue.php
+    // create or open (if exists) the database
+    $db = new SQLite3('queue.db');
 
-  addQueue($vname, $db);
+    addQueue($vname, $db);
 
-  // For some reason, I wanted to note that I am listening to Switchfoot's "Meant to Live."
-  // I know it's not relevant, nor is it a good use of lines of code, but you know what, screw it.
-  // You're not my dad.
-  
-} elseif ($action == 'queue') {
-  // Function library for video_queue.php
-  // create or open (if exists) the database
-  $db = new SQLite3('queue.db');
-  // Return JSON list of queued videos
-  $q = [];
-  $q = readQueue($db);
-  if (gettype($q)=="array") { removeQueue($db, $q['id']); }
-  header('Content-Type: application/json; charset=utf-8');
-  echo json_encode($q);
+    // For some reason, I wanted to note that I am listening to Switchfoot's "Meant to Live."
+    // I know it's not relevant, nor is it a good use of lines of code, but you know what, screw it.
+    // You're not my dad.
+
+    break;
+  case 'queue':
+    // Function library for video_queue.php
+    // create or open (if exists) the database
+    $db = new SQLite3('queue.db');
+    // Return JSON list of queued videos
+    $q = [];
+    $q = readQueue($db);
+    //if (gettype($q)=="array") { removeQueue($db, $q['id']); }
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($q);
+
+    break;
+  case 'remove':
+    if ( !isset($_GET['id']) ) {
+      die('no id provided in GET');
+    }
+    
+    // I know this is heinously insecure but ultimately don't care right now. <3
+    $id = $_GET['id'];
+    
+    // Function library for video_queue.php
+    // create or open (if exists) the database
+    $db = new SQLite3('queue.db');
+    removeQueue($db, $id);
+    header('Content-Type: application/json; charset=utf-8');
+    echo true;
+    break;
 }
